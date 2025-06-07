3f9396240efcc79a399fb12ccb42c7bc062a9001
1в <cykalov.aleksand@mail.ru>
2025-06-07 12:12:43 +0300

    Initial Commit

---
+++ src/main/java/sky/group/homeworkgroup/HomeworkgroupApplication.java
@@ -0,0 +1,13 @@
+package sky.group.homeworkgroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkgroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkgroupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sky/group/homeworkgroup/HomeworkgroupApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.group.homeworkgroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkgroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
