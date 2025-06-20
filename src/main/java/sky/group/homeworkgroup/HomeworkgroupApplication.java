17a8bd518269d1e020e91258b0e68754ac635148
1в <cykalov.aleksand@mail.ru>
2025-06-21 02:13:10 +0300

    предварительный итоговый вариант

--- src/main/java/sky/group/homeworkgroup/HomeworkgroupApplication.java
+++ src/main/java/sky/group/homeworkgroup/HomeworkgroupApplication.java
@@ -3,6 +3,8 @@ package sky.group.homeworkgroup;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
 @OpenAPIDefinition
 @SpringBootApplication
 public class HomeworkgroupApplication {
