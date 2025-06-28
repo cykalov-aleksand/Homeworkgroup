f1e7a01214db31a2b13b110dfad81c700342315d
Mick210 <sid222m@gmail.com>
2025-06-28 11:53:21 +0300

    исправления

--- src/main/java/sky/group/homeworkgroup/controller/CacheController.java
+++ src/main/java/sky/group/homeworkgroup/controller/CacheController.java
@@ -1,18 +1,26 @@
 package sky.group.homeworkgroup.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sky.group.homeworkgroup.service.CacheService;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/management")
 public class CacheController {
     private final CacheService cacheService;
+    private final BuildProperties buildProperties;
 
-    public CacheController(CacheService cacheService) {
+    @Autowired
+    public CacheController(CacheService cacheService, BuildProperties buildProperties) {
         this.cacheService = cacheService;
+        this.buildProperties = buildProperties;
     }
 
     @PostMapping("/clear-caches")
@@ -25,4 +33,11 @@ public class CacheController {
         }
     }
 
+    @GetMapping("/info")
+    public Map<String,String> getServiceInfo() {
+        return Map.of(
+                "name", buildProperties.getName(),
+                "version", buildProperties.getVersion()
+        );
+    }
 }
