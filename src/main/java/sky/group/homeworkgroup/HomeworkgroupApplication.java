293e422eb631aa636bd35fe4f85db33e366e008b
1в <cykalov.aleksand@mail.ru>
2025-07-05 18:58:59 +0300

    проверьте пожалуйста домашнее задание

--- src/main/java/sky/group/homeworkgroup/HomeworkgroupApplication.java
+++ src/main/java/sky/group/homeworkgroup/HomeworkgroupApplication.java
@@ -3,7 +3,7 @@ package sky.group.homeworkgroup;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
+
 
 @OpenAPIDefinition
 @SpringBootApplication
